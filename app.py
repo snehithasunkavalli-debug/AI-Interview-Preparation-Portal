a0ad6f3179bdd7b7ad7753a228720d8c74c34dfd
snehithasunkavalli-debug <snehithasunkavalli@gmail.com>
2026-08-03 23:11:21 +0530

    app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ from google import genai
 
 
 client = genai.Client(
-    api_key = "AQ.Ab8RN6JcurPdtRmY4_vuJv1y8P-sLgREjPztkjMcLOkB8zy-oQ"
+    api_key = "AQ.Ab8RN6JnnhuqLExCh3or5pGXRmljwoxj9Xd2yHxrNZC1cRcGaQ"
 )
-MODEL_NAME = "models/gemini-2.5-flash-latest"
+MODEL_NAME = "gemini-2.5-flash-latest"
 
 app = Flask(__name__, static_folder="static")
 
